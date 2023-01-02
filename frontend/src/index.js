308e38221996eaaf046a7bd5820200aaf1e6e09b
Bruna Ishida <bruishida2107@gmail.com>
2023-01-02 00:38:16 -0300

    remove unnecessary import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,6 @@ import config from "./config";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
